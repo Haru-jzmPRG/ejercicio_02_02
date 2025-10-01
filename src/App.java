0d29aa875c52af739b1d630a34fb1b1ac1a73873
Jazmin <programajazminc20@gmail.com>
2025-10-01 14:13:23 +0200

    feat: write my name

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nombre = "Jazmin Gutierrez Castro";
+
+        System.out.println(nombre);
     }
 }
